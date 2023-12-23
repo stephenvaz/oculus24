b76454f37f645bee708a6467e3a75ddbbd1783e4
Siddhant Rao <siddhant.rao@spit.ac.in>
2023-12-23 17:38:48 +0530

    setup routing and added basic navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,51 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import LandingPage from './Pages/LandingPage';
+import ErrorPage from './Pages/ErrorPage';
+import EventsPage from './Pages/EventsPage';
+import SponsorsPage from './Pages/SponsorsPage';
+import LoginPage from './Pages/LoginPage';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <>
+          <Navbar />
+          <Outlet />
+          <Footer/>
+        </>
+      ),
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: '/login',
+          element: <LoginPage />,
+        },
+        {
+          path: '/',
+          element: <LandingPage />,
+        },
+        {
+          path: '/events',
+          element: <EventsPage />,
+        },
+        {
+          path: '/sponsors',
+          element: <SponsorsPage />,
+        },
+      ],
+    },
+  ]);
 
   return (
-    <>
-      <h1 className="text-cyan-400 text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
+    <div className="h-screen w-screen">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
-export default App
+export default App;
