f901e59d9627961159b7f81505940f7d2a504def
Stephen Vaz <stephenvaz2003@gmail.com>
2024-01-16 18:56:02 +0530

    carousel page

--- client/src/Pages/LandingPage.jsx
+++ client/src/Pages/LandingPage.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
-
+import React, { useRef, useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import LandingData from './landing';
+import './landing.css';
 function LandingPage() {
+  const galleryBoxRef = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: galleryBoxRef.current,
+        // start: "top center-=200px",
+        // end: "bottom+=3000px center-=200px ",
+        // i want a start end such that the gallery_box_outer is always in the center of the screen
+        start: "top 50%",
+        end: "bottom 50%",
+
+        scrub: true,
+        pin: true,
+        markers: true,
+      },
+    });
+
+    tl.to('.gallery_box_outer', {
+      duration: 10,
+      rotateY: 360,
+      ease: "none",
+    });
+  }, []);
+
   return (
-    <div>LandingPage</div>
-  )
+    <div className="work2">
+      <div className="gallery_box" ref={galleryBoxRef}
+      >
+        <div className="gallery_box_outer">
+          {LandingData.map((tile, index) => (
+            <div key={tile.id} className="gallery_box_in" style={{
+              backgroundImage: `url(${tile.image})`,
+              transform: `rotateY(${index * 60}deg) translateZ(450px)`,
+            }}>
+              <a href={tile.href}>
+                <img src={tile.image} alt={tile.altText} />
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
---
+++ client/src/Pages/landing.js
@@ -0,0 +1,50 @@
+const LandingData = [
+  
+    {
+      id: 60,
+      color1: "#f07967",
+      color2: "#adc710",
+      href: "https://cydstumpel.nl/work/studio-sallali/",
+      image: "https://cydstumpel.nl/wp-content/uploads/2022/12/562A1488-1-scaled-1-e1686480340704.jpeg",
+    },
+    {
+      id: 59,
+      color1: "#f07967",
+      color2: "#adc710",
+      href: "https://cydstumpel.nl/work/maison-objet/",
+      image: "https://cydstumpel.nl/wp-content/uploads/2023/01/levitate-e1686480211987.webp",
+    },
+    {
+      id: 58,
+      color1: "#f07967",
+      color2: "#adc710",
+
+      href: "https://cydstumpel.nl/work/maison-objet/",
+      image: "https://cydstumpel.nl/wp-content/uploads/2023/01/hamid.jpeg",
+    },
+    {
+      id: 57,
+      color1: "#f07967",
+      color2: "#adc710",
+      href: "https://cydstumpel.nl/work/maison-objet/",
+      image: "https://cydstumpel.nl/wp-content/uploads/2022/12/wv-scaled-1-e1686480448432.jpeg",
+    },
+    {
+      id: 56,
+      color1: "#f07967",
+      color2: "#adc710",
+      href: "https://cydstumpel.nl/work/maison-objet/",
+      image: "https://cydstumpel.nl/wp-content/uploads/2023/09/Squad2A-Cyd-Co-teacher-1-1-1-1024x648.png"
+
+    },
+    {
+      id: 55,
+      color1: "#f07967",
+      color2: "#adc710",
+      href: "https://cydstumpel.nl/work/maison-objet/",
+      image: "https://cydstumpel.nl/wp-content/uploads/2022/12/expStart_StanKoolen-e1686480424581.jpeg"
+    }
+
+]
+
+export default LandingData;
