61896d0a82072ba8d0352db608e7c97efa6dc584
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2024-01-19 00:39:06 +0530

    cmt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
@@ -7,7 +8,9 @@ import SponsorsPage from './Pages/SponsorsPage';
 import LoginPage from './Pages/LoginPage';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
-import space from './assets/space1.jpg'
+import space from './assets/space1.jpg';
+import { motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
   const router = createBrowserRouter([
@@ -16,61 +19,92 @@ function App() {
       element: (
         <>
           <Navbar />
-          <Outlet />
-          <Footer/>
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -100 }}
+            transition={{ duration: 0.5 }}
+            className='h-full w-full'
+          >
+            <Outlet />
+          </motion.div>
+          <Footer />
         </>
       ),
       errorElement: <ErrorPage />,
       children: [
         {
           path: '/login',
-          element: <LoginPage />,
+          element: (
+            // <motion.div
+            //   initial={{ opacity: 0 }}
+            //   animate={{ opacity: 1 }}
+            //   exit={{ opacity: 0 }}
+            // >
+            <LoginPage />
+            // </motion.div>
+          ),
         },
         {
           path: '/',
-          element: <LandingPage />,
+          element: (
+            // <motion.div
+            //   initial={{ opacity: 0 }}
+            //   animate={{ opacity: 1 }}
+            //   exit={{ opacity: 0 }}
+            // >
+            <LandingPage />
+            // </motion.div>
+          ),
         },
         {
           path: '/events',
-          element: <EventsPage />,
+          element: (
+            // <motion.div
+            //   initial={{ opacity: 0 }}
+            //   animate={{ opacity: 1 }}
+            //   exit={{ opacity: 0 }}
+            // >
+            <EventsPage />
+            // </motion.div>
+          ),
         },
         {
           path: '/sponsors',
-          element: <SponsorsPage />,
+          element: (
+            // <motion.div
+            //   initial={{ opacity: 0 }}
+            //   animate={{ opacity: 1 }}
+            //   exit={{ opacity: 0 }}
+            // >
+            <SponsorsPage />
+            // </motion.div>
+          ),
         },
       ],
     },
   ]);
 
   return (
-    <div
-    // style={{
-    //   backgroundImage: `url(${space})`,
-    //   backgroundRepeat: 'no-repeat',
-    //   backgroundSize: 'cover',
-    //   backgroundPosition: 'center',
-    //   height: '100%',
-    //   width: '100%',
-    //   position: 'fixed',
-    //   top: 0,
-    //   left: 0,
-    //   zIndex: -1,
-    // }}
-    className="h-screen w-full">
-      <img
-      style={{
-        height: '100%',
-        width: '100%',
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        zIndex: -1,
-      }} 
-      src={space} 
-      // src='https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-      alt="" />
-      <RouterProvider router={router} />
-    </div>
+    <AnimatePresence mode='wait'>
+      <div className="h-screen w-full">
+        <img
+          style={{
+            height: '100%',
+            width: '100%',
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+          src={space}
+          alt=""
+        />
+        <div className="h-full w-full">
+          <RouterProvider router={router} />
+        </div>
+      </div>
+    </AnimatePresence>
   );
 }
 
