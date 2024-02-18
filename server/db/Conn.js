a9f14169be92ac9f84926450876105e8a6d56e1e
Stephen Vaz <stephenvaz2003@gmail.com>
2024-02-18 17:39:19 +0530

    dotenv fix

--- server/db/Conn.js
+++ server/db/Conn.js
@@ -2,6 +2,7 @@ import admin from "firebase-admin";
 // import firebaseConfig from "../env/fb.json";
 import service from "../env/service.js";
 import dotenv from "dotenv";
+dotenv.config();
 
 const mAdmin =  admin.initializeApp({
   credential: admin.credential.cert(service),
