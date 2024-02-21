7e9c8bc0bec00a3858b3b4b630d8da5b325a020a
1SatyamJaiswal <satyamjaiswal9752@gmail.com>
2024-02-21 15:58:28 +0530

    started the events page

--- client/src/Pages/EventsPage.jsx
+++ client/src/Pages/EventsPage.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-function EventsPage() {
+const EventsPage = () => {
+  const [events, setEvents] = useState([]);
+  const [selected, setSelected] = useState('All');
+  // useEffect(() => {
+  //   console.log(selected);
+  // },[selected]);
   return (
-    <div className='h-full w-full'>EventsPage</div>
+    <div className='h-full w-full py-28 x-container'>
+      <h1 className='text-5xl neon font-bold mb-7'>Events</h1>
+      <div className='flex flex-row gap-4 flex-wrap'>
+        <button className='custom-btn btn-9' onClick={ () => setSelected('All')}>All</button>
+        <button className='custom-btn btn-9' onClick={() =>setSelected('Pre - Events')}>Pre - Events</button>
+        <button className='custom-btn btn-9' onClick={() =>setSelected('Technical')}>Technical</button>
+        <button className='custom-btn btn-9' onClick={() =>setSelected('Fun')}>Fun</button>
+        <button className='custom-btn btn-9' onClick={() =>setSelected('Cultural')}>Cultural</button>
+      </div>
+    </div>
   )
 }
 
--- client/src/Pages/SchedulePage.jsx
+++ client/src/Pages/SchedulePage.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const SchedulePage = () => {
     return (
-        <div className='h-full w-full'>
-            Schedule Page
+        <div className='h-full w-full py-28 x-container'>
+            <h1 className='text-5xl neon font-bold mb-7'>Schedule</h1>
         </div>
     );
 };
