79ba9f8b75d16110c17580cab44517a9f1957a73
Vi-shub <Smsharma3121@gmail.com>
2024-03-02 02:18:42 +0530

    landing_page done

--- client/src/Pages/LandingPage.jsx
+++ client/src/Pages/LandingPage.jsx
@@ -1,25 +1,35 @@
-import React, { useRef, useEffect } from 'react';
-import LandingData from './landing';
-import space from '../assets/space.png'
-import Appear from '../animated-components/Appear';
-import logo from '../assets/Full White.png'
-import ScrollComp from '../Components/scroll/ScrollComp';
-import voyage from '../assets/cosmic voyage white (stroked).png';
-import Center from '../animated-components/Center';
-import TLeft from '../animated-components/TLeft';
-import Shuffle from '../Components/Shuffle';
+import React, { useRef, useEffect } from "react";
+import LandingData from "./landing";
+import space from "../assets/space.png";
+import Appear from "../animated-components/Appear";
+import logo from "../assets/Full White.png";
+import ScrollComp from "../Components/scroll/ScrollComp";
+import voyage from "../assets/cosmic voyage white (stroked).png";
+import Center from "../animated-components/Center";
+import TLeft from "../animated-components/TLeft";
+import Shuffle from "../Components/Shuffle";
+import Spline from "@splinetool/react-spline";
 
 function LandingPage() {
-
   return (
-    <div className="w-full flex flex-col !items-center !justify-center gap-2">
+    <div className="w-full flex flex-col  gap-2 ">
       <Appear>
-        <div className='h-screen z-[-999] w- flex items-center justify-center select-none'>
-          <img
+        <div className="h-screen z-[-999] w- flex items-center justify-center select-none">
+          {/* <img
             src={logo}
             alt=""
             className='z-10 w-3/4 md:w-1/2 select-none'
-          />
+          /> */}
+          <Spline
+              scene="https://prod.spline.design/5fAYzCZXCfJK1ab5/scene.splinecode"
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 20,
+                zIndex: 3,
+              }}
+            />
         </div>
       </Appear>
       {/* <TLeft>
@@ -31,7 +41,7 @@ function LandingPage() {
       <div className='h-screen w-full flex items-center justify-center'>
         <ScrollComp />
       </div> */}
-      <div className='h-screen w-full flex items-center justify-center mt-4'>
+      <div className="h-screen w-full flex items-center justify-center mt-4">
         <Center>
           <Shuffle />
         </Center>
--- client/src/Pages/SponsorsPage.jsx
+++ client/src/Pages/SponsorsPage.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Spline from '@splinetool/react-spline';
 
 function SponsorsPage() {
   return (
-    <div className='h-full w-full'>SponsorsPage</div>
-  )
+    <div className='h-full w-full flex items-center justify-center'>
+      
+      {/* <Spline scene="https://prod.spline.design/5fAYzCZXCfJK1ab5/scene.splinecode" style={{ position: 'absolute', top: 0, left: 0,right:20, zIndex: 2 }} /> */}
+
+    </div>
+  );
 }
 
-export default SponsorsPage
+export default SponsorsPage;
