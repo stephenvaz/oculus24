f9ce184653d328ffac29a8d72ae3c6750aa9b9ff
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2024-03-09 18:58:08 +0530

    main comp added

--- client/src/Pages/LandingPage.jsx
+++ client/src/Pages/LandingPage.jsx
@@ -1,16 +1,9 @@
-import React, { useRef, useEffect } from 'react';
-import LandingData from './landing';
-import space from '../assets/space.png'
+import React from 'react';
 import Appear from '../animated-components/Appear';
-import logo from '../assets/Full White.png'
-import ScrollComp from '../Components/scroll/ScrollComp';
-import voyage from '../assets/cosmic voyage white (stroked).png';
 import Center from '../animated-components/Center';
-import TLeft from '../animated-components/TLeft';
-import SlideLeft from '../animated-components/SlideLeft';
-import SlideRight from '../animated-components/SlideRight';
 import Shuffle from '../Components/Shuffle';
 import Cosmic from '../Components/Cosmic';
+import Spline from "@splinetool/react-spline"; 
 
 function LandingPage() {
 
@@ -18,10 +11,20 @@ function LandingPage() {
     <div className="w-full flex flex-col !items-center !justify-center gap-2 overflow-x-clip">
       <Appear>
         <div className='h-screen z-[-999] flex items-center justify-center select-none'>
-          <img
+          {/* <img
             src={logo}
             alt=""
             className='z-10 w-3/4 md:w-1/2 select-none'
+          /> */}
+          <Spline
+            scene="https://prod.spline.design/oNjfXSplRKV2iLcL/scene.splinecode"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 20,
+              zIndex: 3,
+            }}
           />
         </div>
       </Appear>
@@ -56,9 +59,6 @@ function LandingPage() {
           </iframe>
         </div>
       </div>
-      {/* <div className='h-screen w-full flex items-center justify-center'>
-        <ScrollComp />
-      </div> */}
     </div>
   );
 }
