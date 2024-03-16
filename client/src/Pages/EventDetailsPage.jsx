b7f0779dea0fc97ee7fc82533397b3ad0e304725
1SatyamJaiswal <satyamjaiswal9752@gmail.com>
2024-03-16 16:17:03 +0530

    Styling fix

--- client/src/Pages/EventDetailsPage.jsx
+++ client/src/Pages/EventDetailsPage.jsx
@@ -18,7 +18,7 @@ const EventDetailsPage = () => {
 
   return (
     <div className='w-full min-h-[screen] relative pb-12'>
-      <div className='w-full h-[400px] sm:h-[600px] overflow-hidden'>
+      <div className='w-full h-[400px] overflow-hidden'>
         <img
           className='w-full h-full object-cover bg-no-repeat'
           src={event.bg_img}
